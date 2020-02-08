a14d3fe4ee8da91a0924c102f8106941a8d1a3ad
Lilian <lilianbour0@gmail.com>
2020-02-08 18:59:45 +0100

    add gitignore and vs files

---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+﻿#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+	Mat img = imread("lena.png");
+	if (!img.data)
+	{
+		std::cout << "Image not loaded";
+		return -1;
+	}
+	namedWindow("image", WINDOW_NORMAL);
+	imshow("image", img);
+	waitKey(0);
+	return 0;
+}
